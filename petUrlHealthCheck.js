1541ec835c851501d83f3a6bb1db9017a406ad9d
Quiza12 <Quiza12@live.com>
2018-05-30 21:25:55 +1000

    Added request-promise to execute async reqeusts to ensure looping urls returns status codes

--- petUrlHealthCheck.js
+++ petUrlHealthCheck.js
@@ -1,32 +1,59 @@
-var request = require('request');
 var rp = require('request-promise');
 var fs = require('fs');
+var cheerio = require('cheerio');
+
+var websiteTarget;
+var startingPoint = 0;
+var endingPoint = 0;
+var targetUrl;
+var targetFolder;
 
 console.log('RSPCA Scraper' + '\n');
 console.log('Processing...');
 
 //Logic
+function setArguments() {
+  websiteTarget = process.argv[2];
+  startingPoint = process.argv[3];
+  endingPoint = parseInt(startingPoint) + 1000;
+  if (websiteTarget === 'adoptapet') {
+    targetUrl = 'https://www.adoptapet.com.au/pet/';
+    targetFolder = './adoptapet-healthy-urls.txt';
+  } else if (websiteTarget === 'rspcansw') {
+    targetUrl = 'https://www.rspcansw.org.au/adopt/pet/';
+    targetFolder = './rspcansw-healthy-urls.txt';
+  }
+  console.log(websiteTarget + ': ' + startingPoint + ' - ' + endingPoint);
+}
+
 function loopUrls() {
-  for (var i = 560000; i <= 565000; i++) {
-    var url = 'https://www.adoptapet.com.au/pet/' + i;
+  for (var i = startingPoint; i <= endingPoint; i++) {
+    var url = targetUrl + i;
     fireRequest(url);
   }
 }
 
 function writeToFile(contents) {
-  var stream = fs.createWriteStream('./healthy-urls.txt', {flags:'a'});
+  var stream = fs.createWriteStream(targetFolder, {flags:'a'});
   stream.write(contents + '\n');
   stream.end();
 }
 
+function scrapeBody(body) {
+  var $ = cheerio.load(body);
+  console.log($('<b>Name:</b>').val());
+}
+
 function fireRequest(url) {
    rp({uri: url, resolveWithFullResponse: true})
-      .then(function (response) {
+      .then(function (response, body) {
       console.log('Valid URL - ' + url);
       writeToFile(url);
+      //scrapeBody(body);
     }).catch(function (reason) {
-      console.log('---NOT VALID - ' + url);
+      console.log('---NOT VALID---' + url);
     });
 }
 
+setArguments();
 loopUrls();
